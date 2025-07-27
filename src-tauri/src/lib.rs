113e0e1d410a0b13c8990640b3124fe8e12f11df
buttfa <1662332017@qq.com>
2025-07-27 21:02:01 +0800

    fix: 修复 Linux 下的白屏问题，并添加调试版本自动打开 Devtools 的功能

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,21 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // 修复 Linux 下的白屏问题
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // 仅在调试(debug)版本中包含此代码
+            {
+                // 自动打开 Devtools
+                use tauri::Manager;
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
